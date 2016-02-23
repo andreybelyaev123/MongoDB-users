733d7d0aad86b3eec4a0e5f8f6b4d0fa4774b3ac
Andreyb <abelyaev@4ds.com>
2016-02-22 23:43:40 -0800

    Added Junit tests

---
+++ src/tests/HttpRequest.java
@@ -0,0 +1,76 @@
+package tests;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class HttpRequest {
+	private static final String urlHead = "http://localhost:9998/UserService/";
+	
+	public static String httpGet(String endPoint, String version) throws IOException {
+		String line;
+		StringBuilder jsonString = new StringBuilder();
+	    HttpURLConnection connection = null;
+	    try {
+	        URL url = new URL(urlHead + endPoint);	        
+
+	        connection = (HttpURLConnection) url.openConnection();
+
+	        connection.setDoInput(true);
+	        connection.setDoOutput(true);
+	        connection.setRequestMethod("GET");
+	        connection.setRequestProperty("Accept", "application/json");
+	        connection.setRequestProperty("Content-Type", "application/mongo-users-"+version +"; charset=UTF-8");
+	        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	        while ((line = br.readLine()) != null) {
+	                jsonString.append(line);
+	        }
+	        br.close();
+	    } catch (IOException e) {
+	            throw new IOException(e.getMessage());
+	    }
+	    finally {
+	    	if (connection != null)
+		        connection.disconnect();
+	    }
+	    return jsonString.toString();
+	}
+	
+	public static String httpPost(String endPoint, String payload, String version) throws IOException {
+		String line;
+		StringBuilder jsonString = new StringBuilder();
+	    HttpURLConnection connection = null;
+	    try {
+	        URL url = new URL(urlHead + endPoint);	        
+
+	        connection = (HttpURLConnection) url.openConnection();
+
+	        connection.setDoInput(true);
+	        connection.setDoOutput(true);
+	        connection.setRequestMethod("POST");
+	        connection.setRequestProperty("Accept", "application/json");
+	        connection.setRequestProperty("Content-Type", "application/mongo-users-"+version +"; charset=UTF-8");
+	        if ( payload != null ) {
+	        	OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
+	        	writer.write(payload);
+	        	writer.close();
+	        }
+	        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	        while ((line = br.readLine()) != null) {
+	                jsonString.append(line);
+	        }
+	        br.close();
+	    } catch (IOException e) {
+	            throw new IOException(e.getMessage());
+	    }
+	    finally {
+	    	if (connection != null)
+		        connection.disconnect();
+	    }
+	    return jsonString.toString();
+	}	
+}
